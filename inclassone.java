af31eed17c00dadf582115d57036d10d8b6288ad
julyaugustusc <32376012+julyaugustusc@users.noreply.github.com>
2021-09-30 14:26:42 -0400

    Final

---
+++ inclassone.java
@@ -0,0 +1,26 @@
+/*import java.util.Scanner;
+public class inclassone {
+	public static void main(String arg[]) {
+		
+		Scanner scan = new Scanner(System.in);
+		int upperCount = 0, lowerCount = 0, digitCount = 0;
+		String string1 = scan.nextLine();
+		
+		for(int i=0; string1.length() - 1; i++) {
+			if(Character.isUpperCase(string1.charAt(i)))
+				upperCount++;
+			else if(Character.isLowerCase(string1.charAt(i)))
+				lowerCount++;
+			else if(Character.isDigit(string1.charAt(i)))
+				digitCount++;
+			
+		
+		}
+			
+		
+		
+		
+		
+	}
+
+}*/
